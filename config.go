f851d1a5e0e16671551714f94d4a51f549f433a9
Jim Weinert <countingtoten@gmail.com>
2018-07-04 17:19:41 -0500

    Fix BaseURL and BindAddress using the same env

--- config.go
+++ config.go
@@ -1,8 +1,8 @@
 package shorty
 
 type Config struct {
-	Address         string `env:"BIND_ADDRESS" envDefault:"localhost"`
+	Address         string `env:"BIND_ADDRESS"`
 	BindPort        int    `env:"BIND_PORT" envDefault:"3000"`
-	BaseURL         string `env:"BIND_ADDRESS" envDefault:"http://localhost:3000/"`
+	BaseURL         string `env:"BASE_URL" envDefault:"http://localhost:3000/"`
 	ShortCodeLength int    `env:"SHORT_URL_CODE_LENGTH" envDefault:"10"`
 }
